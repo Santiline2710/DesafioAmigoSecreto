90260bd9200bb29697a3a888d8f6456931301c41
Santiago Gutierrez <sngutierrez541@gmail.com>
2025-08-04 17:49:51 -0300

    terminado

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+
 // variables
 let listaDeAmigos = [];
 let lista = document.getElementById('listaAmigos');
 
 //funciones
 function agregarAmigo (){
-    let nombre = document.getElementById("amigo").value;
+    let nombre = document.getElementById("amigo").value.trim();
     if (nombre === ""){
         alert("Porfavor inserte un nombre");
     }else {
         actualizarLista(nombre);
     }
     document.getElementById("amigo").value="";
+    
 }
 function actualizarLista(nombre){
     if(listaDeAmigos.includes(nombre)){
@@ -22,5 +24,14 @@ function actualizarLista(nombre){
         listaDeAmigos.push(nombre);
     }
 }
-
+function sortearAmigo(){
+    if (listaDeAmigos.length == 0){
+        alert("no agregaste a ningun amigo");
+    } else{
+        let numAleatori = Math.floor(Math.random()*listaDeAmigos.length);
+        document.getElementById("amigo").value = "";                             
+        lista.innerHTML = '';
+        resultado.innerHTML = `El amigo sorteado es ${listaDeAmigos[numAleatori]}`;
+    }
+}
 
