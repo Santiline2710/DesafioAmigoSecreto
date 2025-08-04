013a014ce71d976d8026cbca0f2c391373ae3a5c
Santiago Gutierrez <sngutierrez541@gmail.com>
2025-08-04 16:04:20 -0300

    actualizar app

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log("verificacion");
+// variables
+let listaDeAmigos = [];
+
+
+//funciones
+function agregarAmigo (){
+    let amigo = document.getElementById("amigo").value;
+    //verificacion en consola
+    console.log("tipeo de usuario:" + amigo);
+    console.log(listaDeAmigos);
+    
+    //verifica que el usuario tipea algun nombre
+    if (amigo == ""){
+        alert("Porfavor inserte un nombre");
+    }else {
+        listaDeAmigos.push(amigo);
+    }
+    document.getElementById("amigo").value="";
+}
+
+
